4c12a5ca44a27e8c9823402847110e91c9726dbb
lile <lile@sysware.com.cn>
2023-08-10 11:34:32 +0800

    log随机颜色

--- bin/start.js
+++ bin/start.js
@@ -1,11 +1,21 @@
 var nodemon = require('nodemon');
 const chalk = require('chalk');
 
+const readyStr = 'fake API';
+
 var checkUpdate = require('../common/checkUpdate');
 checkUpdate();
 
 
 nodemon({ script: './bin/www' })
     .on('start', function () {
-        console.log(`${chalk.hex('#FF0000')('f')}${chalk.hex('#FF7F00')('a')}${chalk.hex('#FFFF00')('k')}${chalk.hex('#00FF00')('e')} ${chalk.hex('#00FFFF')('A')}${chalk.hex('#0000FF')('P')}${chalk.hex('#8B00FF')('I')} ${chalk.gray('is')} ${chalk.bold.green('ready')}`);
+
+        let logger = ''
+
+        for (let i = 0; i < readyStr.length; i++) {
+            const color = '#' + Math.random().toString(16).substr(2, 6);
+            logger += chalk.bold.hex(color)(readyStr[i])
+        }
+        logger += chalk.green(' is ready')
+        console.log(logger);
     })
